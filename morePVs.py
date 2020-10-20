91aac06d5a35cba8da483caaff05b979e08b98c7
mike-b-roberts <m.roberts@unsw.edu.au>
2020-10-20 20:00:03 +1100

    volumetric analysis

--- morePVs.py
+++ morePVs.py
@@ -2662,7 +2662,7 @@ if __name__ == "__main__":
     default_base_path = 'C:\\Users\\z5044992\\Documents\\python\\morePVs\\DATA_EN_MR1'  #(Mike's PC)
     # default_base_path = '/Users/mikeroberts/Documents/python/morePVs/DATA_EN_MR1'  # (Mike's Mac)
     default_project = 'latest24-9'
-    default_study = 'demo_wstc'
+    default_study = 'demo_all_volumetric'
     # default_base_path = '/Users/mikeroberts/OneDrive - UNSW/python/en/DATA_EN_6M'  #(Mike's Mac)
 
     # Import arguments - allows multi-processing from command line
